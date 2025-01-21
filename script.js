c5d52da5f80cd85247f28525a53bebe808db104f
Imajran02 <ajranhudaa@gmail.com>
2025-01-21 14:20:39 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Contoh JavaScript untuk interaktivitas seperti tombol scroll atau animasi
+document.querySelector("button").addEventListener("click", function() {
+    alert("Selamat datang di dunia investasi cryptocurrency!");
+});
